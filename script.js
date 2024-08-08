d171c3cd4462d8b110887478d6e9cb8f231bbde3
hideki123123 <105948160+hideki123123@users.noreply.github.com>
2024-08-08 07:55:20 -0300

    sdfsdf

---
+++ script.js
@@ -0,0 +1,20 @@
+const caixaprincipal = document.querySelector('caixa-principal');
+const caixaperguntas = document.querySelector('caixa-perguntas');
+const caixaalternativas = document.querySelector('caixa-alternativas');
+const caixaresultados = document.querySelector('texto-resultado')
+
+const perguntas = [
+    {
+        enunciado: "No âmbito social, você prefere:",
+        alternativas: ["alternativa 1", "alternativa 2"]
+    }
+    { enunciado: "No âmbito ambiental, você prefere:",
+        alternativas:["alternativa 3, alternativa 4"]
+    }
+    {
+        enunciado: " No âmbito tecnológico, você prefere:",
+        alternativas: ["alternativa 1, alternativa 2"]
+    }
+      
+    
+]
