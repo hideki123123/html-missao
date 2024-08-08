8f03255a4141b2ef82d186f5c5bf838e7c5345b7
hideki123123 <105948160+hideki123123@users.noreply.github.com>
2024-08-08 08:40:45 -0300

    a

--- script.js
+++ script.js
@@ -7,12 +7,13 @@ const perguntas = [
     {
         enunciado: "No âmbito social, você prefere:",
         alternativas: ["alternativa 1", "alternativa 2"]
-    
-     
+    }
+     {
         enunciado: "No âmbito ambiental, você prefere:",
         alternativas:["alternativa 3, alternativa 4"]
     
-    
+     }
+     {    
         enunciado: " No âmbito tecnológico, você prefere:",
         alternativas: ["alternativa 1, alternativa 2"]
     } 
