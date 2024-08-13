bea65e6c9c182d793f1750b279527a9a23fc302c
hideki123123 <105948160+hideki123123@users.noreply.github.com>
2024-08-13 08:40:34 -0300

    sdd

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ function mostraAlternativas(){
     for (const alternativas of perguntaAtual.alternativas) {
         const botaoAlternativa = document.createElement("button");
         botaoAlternativa.textContent = alternativas.texto;
-        botaoAlternativa.addEventListener("click", FUNCTION()+{
+        botaoAlternativa.addEventListener("click", function(){
             atual++;
             mostrarPerguntas();
         })
