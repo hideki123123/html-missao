244fbb456d713897bad197f879a7dc8d24c9764a
hideki123123 <105948160+hideki123123@users.noreply.github.com>
2024-08-20 08:03:46 -0300

    rtrt

--- script.js
+++ script.js
@@ -1,40 +1,51 @@
-const caixaprincipal = document.querySelector('.caixa-principal');
-const caixaperguntas = document.querySelector(".caixa-perguntas");
-const caixaalternativas = document.querySelector('.caixa-alternativas');
-const caixaResultado = document.querySelector("caixaResultado");
-const caixaresultados = document.querySelector('.texto-resultado');
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textResultado = document.querySelector(".texto-resultado")
 
-const perguntas = [
+const perguntas= [
     {
         enunciado: "No âmbito social, você prefere:",
         alternativas: [
             {
-                texto: "você vai ter muito dinheiro, mas",
-                afirmacao: "só pode gastar esse dinheiro no país Butão",
-            }
-
-        ]
+                texto:"voce tera amigos confiaveis mas,",
+                afirmacao: "voce nao pode pedir nada para eles",
+            },
+            {
+                texto:"o tiktok pararia de existir mas,",
+                afirmacao:"nada de ruim acontece",
+            },
+    ]
     },
     {
         enunciado: "No âmbito ambiental, você prefere:",
         alternativas: [
             {
-                texto: "árvores nescem a cada óbito humano, mas",
-                afirmacao: "você é o primeiro a morrer",
-            }
-        ]
-        },
-        {
-        enunciado: " No âmbito tecnológico, você prefere:",
+                texto:"a natureza se restaura mas,",
+                afirmacao: "o seu 3° @ falece",
+            },
+            {
+                texto:"as tartarugas poderiam usar canudos para tomar agua mas,",
+                afirmacao:"elas nao parariam de tomar até acabar",
+            },
+    ]
+    },
+    {
+        enunciado: "No âmbito tecnológico, você prefere:",
         alternativas: [
             {
-                texto: "ter o melhor pc do mercado, mas",
-                afirmacao: "você não pode usar nem teclado e nem mouse",
-            }
-        ]
-    },
-
+                texto:"elon musk te da todo o dinheiro dele, porém,",
+                afirmacao: "voce morre",
+            },
+            {
+                texto:"fortnite é real mas...",
+                afirmacao:"fortnite é real",
+            },
+    ]
+    },   
 ]
+
 let atual = 0;
 let perguntaAtual;
 let historiaFinal = "";
@@ -49,13 +60,13 @@ function mostraAlternativas (){
     for (const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativa = document.createElement("button");
         botaoAlternativa.textContent = alternativa.texto;
-        botaoAlternativa.addEventListener("click", ()=> opcaoSelecionada(afirmacao));
+        botaoAlternativa.addEventListener("click", ()=> opcaoSelecionada(alternativa));
         caixaAlternativas.appendChild(botaoAlternativa);
     }
 
     function respostaSelecionada (opcaoSelecionada){
         const afirmacoes = opcaoSelecionada.afirmacoes;
-
+historiaFinal = alternativa;
         atual++;
         mostraPerguntas()
     }
